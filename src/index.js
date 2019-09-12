fabd295f79f316fadab70848450e19d483900335
yaelBrown <yaelrbrown@gmail.com>
2019-09-12 13:32:15 -0500

    cropped and renamed pug picture. styled card-rows and created logic for rows.

--- src/index.js
+++ src/index.js
@@ -88,27 +88,56 @@ const removeMovie = (id) => {
 
 
 
+let moviesCounter = 0;
 
 // get movies function
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
-
+  
   // fetch movie data
   fetch('../api/movies');
   movies.forEach(({title, rating, id}, i) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
     movieData += `<p>id#${id} - ${title} - rating: ${rating} - <a onClick="removeMovie(${id})">DELETE</a></p>`;
 
-    // Add data to cards
-    cardData += `<div class="card">`
-    cardData += `<h3 class="card-title">${title}</h3>`
-    cardData += `<h2 class="card-rating">${rating}</h2>`
-    cardData += `<div class="card-btns">`
-    cardData += `<i onclick="editMovie()" class="fas fa-edit"></i>`
-    cardData += `<i onclick="removeMovie(${id})" class="fas fa-times-circle"></i>`
-    cardData += `</div>`
-    cardData += `</div>`
+    // Increment counter
+    moviesCounter++;
 
+    console.log(moviesCounter);
+
+    // Add data to cards
+
+    if (moviesCounter === 1) {
+      cardData += `<div class="card-row">`
+      cardData += `<div class="card">`
+      cardData += `<h3 class="card-title">${title}</h3>`
+      cardData += `<h2 class="card-rating">${rating}</h2>`
+      cardData += `<div class="card-btns">`
+      cardData += `<i onclick="editMovie(${id})" class="fas fa-edit"></i>`
+      cardData += `<i onclick="removeMovie(${id})" class="fas fa-times-circle"></i>`
+      cardData += `</div>`
+      cardData += `</div>`
+    } else if (moviesCounter === 3) {
+      cardData += `<div class="card">`
+      cardData += `<h3 class="card-title">${title}</h3>`
+      cardData += `<h2 class="card-rating">${rating}</h2>`
+      cardData += `<div class="card-btns">`
+      cardData += `<i onclick="editMovie(${id})" class="fas fa-edit"></i>`
+      cardData += `<i onclick="removeMovie(${id})" class="fas fa-times-circle"></i>`
+      cardData += `</div>`
+      cardData += `</div>`
+      cardData += `</div>`
+      moviesCounter = 0;
+    } else {
+      cardData += `<div class="card">`
+      cardData += `<h3 class="card-title">${title}</h3>`
+      cardData += `<h2 class="card-rating">${rating}</h2>`
+      cardData += `<div class="card-btns">`
+      cardData += `<i onclick="editMovie(${id})" class="fas fa-edit"></i>`
+      cardData += `<i onclick="removeMovie(${id})" class="fas fa-times-circle"></i>`
+      cardData += `</div>`
+      cardData += `</div>`
+    }
 
   });
 }).catch((error) => {
