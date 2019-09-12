22d8a2ca7c9b990bfb2a01caea3cf850554b5382
yaelBrown <yaelrbrown@gmail.com>
2019-09-11 21:05:25 -0500

    pushing to work from PC

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ const {getMovies} = require('./api.js');
 
 const loading = document.getElementById("loading");
 const container = document.getElementById("container");
+const test = document.getElementById("test");
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   fetch('../db.json');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+    test.innerHTML = `<p>id#${id} - ${title} - rating: ${rating}<p>`;
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
